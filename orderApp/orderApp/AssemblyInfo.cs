5397aaa8c9109890c9151ca3c1425956e444c4b4
Luqman Akram <luqmanakram00300@outlook.com>
2020-10-14 06:23:27 -0700

    Add project files.

---
+++ orderApp/orderApp/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Roboto-Regular.ttf", Alias = "ThemeFont")]
+[assembly: ExportFont("Roboto-Medium.ttf", Alias = "ThemeFontMedium")]
+[assembly: ExportFont("Roboto-Bold.ttf", Alias = "ThemeFontBold")]
---
+++ orderApp/orderApp/Models/CarouselModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace orderApp.Models
+{
+    class CarouselModel
+    {
+        public string Details { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
